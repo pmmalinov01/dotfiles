1bc59bc397cbf06e4b337373ad5e58a005554ee3
Pavel Malinov <pmmalinov01@gmail.com>
2022-09-12 18:56:09 +0300

    removign stuff

--- lua/pavelm/tflint.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, tflint = pcall(require, "tflint")
-if not status_ok then
-  return
-end
-
-
-tflint.setup {}
--- lua/pavelm/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = "all",
-  auto_install = true,
-  highlight = { enable = true },
-  indent = { enable = true }
-}
